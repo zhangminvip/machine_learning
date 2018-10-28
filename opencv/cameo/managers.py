c28ba6a8f79b547a02801b6f405210a83edf2c46
minzhang <a991391009@gmail.com>
2018-10-28 18:07:47 +0800

    extend managers.py

--- opencv/cameo/managers.py
+++ opencv/cameo/managers.py
@@ -19,6 +19,7 @@ class CaptureManager(object):
         self._videoEncoding = None
         self._videoWriter = None
         self._startTime = None
+        self._framesElapsed = long(0)
         self._fpsestimate = None
 
 
@@ -46,6 +47,44 @@ class CaptureManager(object):
     @property
     def isWritingVideo(self):
         return self._videoFilename is not None
+
+    def enterFrame(self):
+        'Capture the next frame ,if any'
+
+        #But first, check that any previous frame was exited
+        assert not self._enteredFrame,\
+        'previous enterFrame() had no matching exitFrame()'
+        if self._capture is not None:
+            self._enteredFrame = self._capture.grab()
+
+    def exitFrame(self):
+        '''
+        Draw to the window, write to the files, Release the frame
+        '''
+        #Check weather any grabbed frame is retrievable
+        #The getter may retrieve and  cache the frame
+
+        if self.frame is None:
+            self._enteredFrame = False
+            return
+
+        #Update the FPS estimate and related variables
+        if self._framesElapsed == 0:
+            self._startTime = time.time()
+        else:
+            timeElapsed = time.time() -self._startTime
+            self._fpsestimate = self._framesElapsed/timeElapsed
+        self._framesElapsed += 1
+
+        #Drop the window , if any
+        if self.previeWindowManager is not None:
+            if self.shouldMirrorPreview:
+                mirroredFrame = numpy.fliplr(self._frame).copy()
+                self.previeWindowManager.show(mirroredFrame)
+            else:
+                self.previeWindowManager.show(self._frame)
+
+
 
 
 
