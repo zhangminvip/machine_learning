b1d73cf3b3b0517ff9656d8edac973adfa979f99
minzhang <a991391009@gmail.com>
2018-10-31 23:42:05 +0800

    add static image detection

--- opencv/face_recognition/static_image.py
+++ opencv/face_recognition/static_image.py
@@ -2,5 +2,18 @@ import cv2
 
 filename = '/home/minzhang/ml/hpf_test.jpg'
 
+
 def detect(filename):
-    
+    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
+    # cv2.namedWindow('beauty detected')
+    cv2.imshow('face detected',img)
+    cv2.imwrite('./face_detected.jpg',img)
+    cv2.waitKey()
+
+
+detect(filename)
