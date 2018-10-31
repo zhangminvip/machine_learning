6f88ffc248cf5f4e95e4978aeed23eb240020176
minzhang <a991391009@gmail.com>
2018-11-01 00:40:36 +0800

    Face detection in video

--- opencv/face_recognition/static_image.py
+++ opencv/face_recognition/static_image.py
@@ -9,10 +9,10 @@ def detect(filename):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     # cv2.namedWindow('beauty detected')
-    cv2.imshow('face detected',img)
-    cv2.imwrite('./face_detected.jpg',img)
+    cv2.imshow('face detected', img)
+    cv2.imwrite('./face_detected.jpg', img)
     cv2.waitKey()
 
 
