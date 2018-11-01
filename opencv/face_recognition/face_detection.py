ddedfb6d0c07c991425ea89b9db3bfd1f0785eb5
minzhang <a991391009@gmail.com>
2018-11-01 14:46:03 +0800

    Face recognition based on eigenface

--- opencv/face_recognition/face_detection.py
+++ opencv/face_recognition/face_detection.py
@@ -26,6 +26,7 @@ def detect():
 
 
 def read_images(path, sz=None):
+
     c = 0
     X, y = [], []
     for dirname, dirnames, filenames in os.walk(path):
@@ -44,10 +45,46 @@ def read_images(path, sz=None):
                 except IOError:
                     print('IO error({0}{1})'.format(IOError.errno, IOError.strerror))
                 except:
-                    print('unexcept error',sys.exc_info()[0])
+                    print('unexcept error', sys.exc_info()[0])
                     raise
             c += 1
-    return [X,y]
+    return [X, y]
 
 
-detect()
+def face_rec():
+    names = ['minzhang']
+    if len(sys.argv) < 2:
+        print('USAGE: facerec_demo.py <path to images> [path to store images at]')
+        sys.exit()
+    [X, y] = read_images(sys.argv[1])
+    print(X)
+    print(y)
+    y = np.asarray(y, dtype=np.int32)
+    if len(sys.argv) == 3:
+        outdir = sys.argv[2]
+    model = cv2.face.EigenFaceRecognizer_create()
+    model.train(np.asarray(X), np.asarray(y))
+    camera = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+    while (True):
+        read, img = camera.read()
+        faces = face_cascade.detectMultiScale(img, 1.3, 5)
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            roi = gray[x:x + w, y:y + h]
+            try:
+                roi = cv2.resize(roi, (200, 200), interpolation=cv2.INTER_LINEAR)
+                params = model.predict(roi)
+                cv2.putText(img, names[params[0]], (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
+            except:
+                continue
+        cv2.imshow('camera', img)
+        if cv2.waitKey(int(1000 / 12)) & 0xff == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+
+# detect()
+
+face_rec()
