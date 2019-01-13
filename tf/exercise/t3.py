2e9f5ed75471aa07d60835d8452cdc69025c18f3
minzhang <a991391009@gmail.com>
2019-01-14 00:16:52 +0800

    specified type

--- tf/exercise/t3.py
+++ tf/exercise/t3.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-a = tf.constant([1.0, 2.0], name='a')
+a = tf.constant([1, 2], name='a', dtype=tf.float32)
 b = tf.constant([2.0, 3.0], name='b')
 result = tf.add(a, b, name='add')
 print(result)
