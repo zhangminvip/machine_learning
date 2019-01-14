c6a3b049dc23c2e79cf7fee104b6b8e16aa289d2
minzhang <a991391009@gmail.com>
2019-01-14 20:53:11 +0800

    set parameter trainable=True , even if the default is True

--- tf/exercise/c3/neural_network_sample.py
+++ tf/exercise/c3/neural_network_sample.py
@@ -5,8 +5,8 @@ from numpy.random import RandomState
 batch_size = 8
 
 
-w1 = tf.Variable(tf.random_normal((2,3), stddev=1, seed=1))
-w2 = tf.Variable(tf.random_normal((3,1), stddev=1, seed=1))
+w1 = tf.Variable(tf.random_normal((2,3), stddev=1, seed=1), trainable=True)
+w2 = tf.Variable(tf.random_normal((3,1), stddev=1, seed=1), trainable=True)
 
 x = tf.placeholder(tf.float32, shape=(None, 2), name='x-input')
 y_ = tf.placeholder(tf.float32, shape=(None,1), name='y-input')
